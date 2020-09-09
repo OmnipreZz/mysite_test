c133c2e63bd20dc96aafa1acfae74bf2112fe074
Omni <geoffrey.frioli@gmail.com>
2020-09-09 18:00:45 +0200

    refresh

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -15,7 +15,7 @@ function About() {
           <div className="AboutInfoText">
             <p>Bonjour, je m'appelle Geoffrey, j'ai 35 ans et je suis passionné par la photographie et l'informatique.</p>
             <p>Le développement web me permet aujourd'hui de mêler ces deux passions avec un certain goût pour le design.</p>
-            <p>Après un an de formation à l'Ecole Régionale du Numérique de St-Gaudens, j'ai obtenu le titre professionnel de développeur web et web mobile de niveau III (bac +2).</p>
+            <p>Après un an de formation à l'Ecole Régionale du Numérique de St-Gaudens, j'ai obtenu le titre professionnel de développeur web et web mobile.</p>
             <p>J'ai ensuite effectué un stage de six mois au sein de l'entreprise Cloub Technologies, où j'ai pu continuer à me former et développer la partie front-end de leur application sur React.js.</p> 
             <p>Actuellement à la recherche d'un premier emploi orienté front-end, je suis motivé à continuer mon apprentissage en ce sens, tout en étant ouvert à toutes technologies nouvelles qui pourraient m'apporter plus d'expérience dans mon métier.</p>
             <p >Autodidacte, curieux et rigoureux, je peux facilement m'adapter à toutes les situations.</p>
